1c77b95af7c830764f409ed244f3e0208c8a153e
kimmydoray <edzelabliter@gmail.com>
2023-08-02 00:15:04 +0800

    [ADD] new function for post request

--- app/Http/Controllers/ConsolidatedController.php
+++ app/Http/Controllers/ConsolidatedController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Consolidated;
 use League\Csv\Reader;
 use League\Csv\Statement;
+use League\Csv\Writer;
 
 use Carbon\Carbon;
 
@@ -19,6 +20,17 @@ class ConsolidatedController extends Controller
         // Create a CSV reader instance
         $csv = Reader::createFromPath($csvFilePath, 'r');
         
+        // $newColumnName1 = 'Total Match';
+        // $header = $csv->getHeader();
+        // $header[] = $newColumnName1;
+        // $csvUpdated->insertOne($header);
+
+        
+        // $csv->insertBeforeHeader($newColumnName1);
+
+        // $newColumnName2 = 'Asset Types';
+        // $csv->insertBeforeHeader($newColumnName2);
+
         // Fetch the header record
         $headers = $csv->getHeader();
         
@@ -56,7 +68,63 @@ class ConsolidatedController extends Controller
        
     }
 
+
+    // CHECK MATCH BETWEEN CSV AND DATABASE
     public function checkMatchData(Request $request) {
+        if ($request->hasFile('csv_file')) {
+            // Get the uploaded file from the request
+            $uploadedFile = $request->file('csv_file');
+
+            // Get the file path of the uploaded file
+            $filePath = $uploadedFile->getRealPath();
+            
+            // Create a CSV reader instance
+            $csv = Reader::createFromPath($filePath, 'r');
+            
+            $newColumnName1 = 'Total Match';
+            $csv->insertBeforeHeader($newColumnName1);
+
+            $newColumnName2 = 'Asset Types';
+            $csv->insertBeforeHeader($newColumnName2);
+
+            // Fetch the header record
+            $headers = $csv->getHeader();
+            
+            // Set the header as the associative array keys
+            $csv->setHeaderOffset(1);
+            
+            // Loop through the CSV records one by one
+            $count = 0;
+            $total = 0;
+            foreach ($csv->getRecords() as $record) {
+                if ($count <= 7) {
+                    //GET COMPARE DATES
+                    $origDate = Carbon::parse($record['In Service Date']);
+                    $startDate = $origDate->subDays(90)->toDateString();
+                    $endDate = $origDate->addDays(90)->toDateString();
+
+                    //GET COMPARE PERCENT
+
+                    $bookBasis = str_replace(',', '', "395.00");
+                    $startCost = ($bookBasis * 0.05);
+                    $startCost = $bookBasis - $startCost;
+                    $endCost = ($bookBasis * 0.05) + $bookBasis;
+
+                    // echo $startCost . "<br>" . $endCost;
+
+                    $data = Consolidated::whereBetween('u_acquisition_date', [$startDate, $endDate])
+                        ->whereBetween('acquisition_cost', [$startCost, $endCost])->get();
+                    $total = $total + count($data);
+                    if(count($data)) {
+                        
+                    }
+                    $count++;
+                } else {
+                    return response()->json(['match' => $total]);
+                }
+            }
+        }
+
         return response()->json(['data' => $request]);
     }
 }
